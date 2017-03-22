8d38eb6448717c2cdd15c93e37438c6bfa02039e
Gustavo Cornejo <gcornejo85@gmail.com>
2017-03-21 21:30:12 -0700

    basic setup for endpoint

---
+++ endpoint.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource,Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class GoldEndpoint(Resource):
+
+    def get(self, quarter, subject):
+        return {quarter:subject}
+
+api.add_resource(GoldEndpoint,'/goldapi/<quarter>/<subject>')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
