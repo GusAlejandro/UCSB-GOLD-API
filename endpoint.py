56569d22a6b3600c411c7cf88ce4fe87cd1bd7b5
Gustavo Cornejo <gcornejo85@gmail.com>
2017-03-27 22:14:26 -0700

    get_course_listing almost completed

--- endpoint.py
+++ endpoint.py
@@ -3,9 +3,12 @@ from flask_restful import Resource,Api
 
 app = Flask(__name__)
 api = Api(app)
+# TODO: Consider using more user-friendly codes for quarters and subjects on user side, keep hash table on server side ?
 
 
 class GoldEndpoint(Resource):
+    # TODO: Once get_course_listing is implemented, modify get request to return json response to client
+    # TODO: Implement error catching when user sends in invalid quarter, course subject, or login credentials
 
     def get(self, quarter, subject):
         return {quarter:subject}
--- scraper.py
+++ scraper.py
@@ -1,17 +1,20 @@
 from settings import authentication
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
 
 
 class Scraper:
 
     def __init__(self):
         self.browser = webdriver.Chrome()
-        self.searchPayload = None
+        self.payload = {}
 
     def get_webpage(self,url):
         self.browser.get(url)
 
     def login(self):
+        # TODO: Implement try and catch to return error message in case of invalid login credentials
         self.get_webpage('https://my.sa.ucsb.edu/gold/Login.aspx')
         user = self.browser.find_element_by_name('ctl00$pageContent$userNameText')
         password = self.browser.find_element_by_name('ctl00$pageContent$passwordText')
@@ -20,17 +23,33 @@ class Scraper:
         self.browser.find_element_by_name('ctl00$pageContent$loginButton').click()
 
 
+
     def parse_course_listings(self, raw_html):
-        pass
+        # TODO: Efficiently parse each class and implement Json packaging for return
+        soup = BeautifulSoup(raw_html, 'html.parser')
+        a = soup.find_all("span",class_='tableheader')
+        for x in a:
+            x = str(x.text)
+            print(x.split())
+
 
     def get_course_listings(self, quarter, subject):
         self.login()
         self.get_webpage('https://my.sa.ucsb.edu/gold/BasicFindCourses.aspx')
-        web = self.browser.page_source
-        self.browser.close()
-        return web
+        quarterField = Select(self.browser.find_element_by_name('ctl00$pageContent$quarterDropDown'))
+        quarterField.select_by_value(quarter)
+        subjectField = Select(self.browser.find_element_by_name('ctl00$pageContent$subjectAreaDropDown'))
+        subjectField.select_by_value(subject)
+        self.browser.find_element_by_name('ctl00$pageContent$searchButton').click()
+        text = self.browser.page_source
+        self.browser.quit()
+        self.parse_course_listings(text)
+        self.browser.quit()
+
+
 
     def get_course_information(self, course):
         pass
 
-
+it = Scraper()
+it.get_course_listings('20171', 'EARTH')
